f31eb3e65ee9374c9f7508fa022eea070b664b21
Ross Burton <ross@burtonini.com>
2008-11-04 22:23:16 +0000

    Extend set-password to set every option

--- set-password.py
+++ set-password.py
@@ -1,23 +1,50 @@
 #! /usr/bin/python
 
-import gobject, gnomekeyring
-
+import sys, gobject, gnomekeyring
 from optparse import OptionParser
-parser = OptionParser(add_help_option=False)
-parser.add_option("-u", "--user", dest="username",
-                  help="The user name", metavar="USER")
-parser.add_option("-h", "--host", dest="hostname",
-                  help="The host name", metavar="HOST")
-parser.add_option("-p", "--password", dest="password",
-                  help="The password", metavar="PASSWORD")
-parser.add_option("-?", "--help", action="help", help="show this help message and exit")
-
-(options, args) = parser.parse_args()
 
 gobject.set_application_name("offlineimap")
 
+parser = OptionParser(add_help_option=False)
+parser.add_option("-u", "--user", dest="user",
+                  help="The user name", metavar="USER")
+parser.add_option("-d", "--domain", dest="domain",
+                  help="The domain", metavar="DOMAIN")
+parser.add_option("-s", "--server", dest="server",
+                  help="The server", metavar="SERVER")
+parser.add_option("-o", "--object", dest="object",
+                  help="The remote object", metavar="OBJECT")
+parser.add_option("-c", "--protocol", dest="protocol",
+                  help="The protocol", metavar="PROTOCOL")
+parser.add_option("-a", "--authtype", dest="authtype",
+                  help="The authentication type", metavar="TYPE")
+parser.add_option("-p", "--port", dest="port", type="int",
+                  help="The port", metavar="PORT")
+parser.add_option("-w", "--password", dest="password",
+                  help="The password", metavar="PASSWORD")
+parser.add_option("-?", "--help", action="help", help="show this help message and exit")
+
+parser.set_defaults(user=None,
+                    domain=None,
+                    server=None,
+                    object=None,
+                    protocol=None,
+                    authtype=None,
+                    port=0,
+                    password=None)
+(options, args) = parser.parse_args()
+
+# This is the only required argument
+if options.password is None:
+    parser.print_help()
+    sys.exit(1)
+
 gnomekeyring.set_network_password_sync(
-    protocol="imap",
-    server=options.hostname,
-    user=options.username,
+    user=options.user,
+    domain=options.domain,
+    server=options.server,
+    object=options.object,
+    protocol=options.protocol,
+    authtype=options.authtype,
+    port=options.port,
     password=options.password)
