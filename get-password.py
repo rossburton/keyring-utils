c2773a82ee49dde774401768b8f6b9876ccc59b4
Ross Burton <ross@burtonini.com>
2010-09-22 14:56:58 +0100

    Sync get-password options with set-password, pretty-print all results

--- get-password.py
+++ get-password.py
@@ -1,20 +1,22 @@
 #! /usr/bin/python
 
-import gobject, gnomekeyring
+import gobject, gnomekeyring, pprint
 
 from optparse import OptionParser
 parser = OptionParser(add_help_option=False)
-parser.add_option("-u", "--user", dest="username",
+parser.add_option("-u", "--user", dest="user",
                   help="The user name", metavar="USER")
-parser.add_option("-h", "--host", dest="hostname",
-                  help="The host name", metavar="HOST")
+parser.add_option("-s", "--server", dest="server",
+                  help="The server", metavar="SERVER")
+parser.add_option("-c", "--protocol", dest="protocol",
+                  help="The protocol", metavar="PROTOCOL")
 parser.add_option("-?", "--help", action="help", help="show this help message and exit")
 
 (options, args) = parser.parse_args()
 
 gobject.set_application_name("keyring-utils")
 
-keys = gnomekeyring.find_network_password_sync(user=options.username,
-                                              server=options.hostname,
-                                              protocol="imap")
-print keys[0]["password"]
+keys = gnomekeyring.find_network_password_sync(user=options.user,
+                                              server=options.server,
+                                              protocol=options.protocol)
+pprint.pprint(keys)
